cba8957fd9d24095b348e21a32b5be7f0ce2faf1
Alexander Lindgren <alexanderlindgren@Alexanders-MacBook-Pro.local>
2021-08-02 19:40:46 +0200

    updated dotenv import placement

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -12,13 +12,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Loading dotenv if there is an .env variable (project run locally)
-if '.env' in os.listdir(BASE_DIR/'myproject'): load_dotenv()
+if '.env' in os.listdir(BASE_DIR/'myproject'): 
+    from dotenv import load_dotenv
+    load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
