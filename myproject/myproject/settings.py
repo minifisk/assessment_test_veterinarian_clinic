902fcad88a2e3083d99651a035878e79944c172a
Alexander Lindgren <alexanderlindgren@Alexanders-MacBook-Pro.local>
2021-08-02 16:23:01 +0200

    updated secret key and set to env variable

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vo=i@%e(n$uf+oyqwct6-q6cpaa*b=z_dd10o)(k*%^*8x+4e*'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
