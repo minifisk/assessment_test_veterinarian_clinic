85f446911f21af3603d56e0f2fc5677c2567ec70
Alexander Lindgren <alexanderlindgren@Alexanders-MacBook-Pro.local>
2021-08-02 19:39:06 +0200

    added psycopg-binary

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -12,17 +12,19 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Loading dotenv if there is an .env variable (project run locally)
+if '.env' in os.listdir(BASE_DIR/'myproject'): load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "bwlq)ed^e8ah2t7k^$i1bdeu5@g4vk&94(3-e(5ve1qe=n@t$u"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
